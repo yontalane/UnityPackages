3156971220fb2799ea213a232e2e9ef62a8c0d73
David Frankel <github@davidfrankel.com>
2025-08-07 01:58:22 -0700

    Fixed typo in obsolete message.

--- Packages/com.yontalane.dialog/Runtime/DialogUI.cs
+++ Packages/com.yontalane.dialog/Runtime/DialogUI.cs
@@ -11,7 +11,7 @@ namespace Yontalane.Dialog
     /// <summary>
     /// Main UI controller for displaying and managing dialog interactions, including speaker display, text typing, responses, and associated UI elements.
     /// </summary>
-    [Obsolete("DialogUI has been moved to a separate package and will be removed from the Dialog package in a future update. Please replace thie component in your project with the DialogUI component found in the DialogUGUI package.")]
+    [Obsolete("DialogUI has been moved to a separate package and will be removed from the Dialog package in a future update. Please replace this component in your project with the DialogUI component found in the DialogUGUI package.")]
     [DisallowMultipleComponent]
     [AddComponentMenu("Yontalane/Dialog/Dialog UI")]
     public sealed class DialogUI : Singleton<DialogUI>
@@ -300,7 +300,7 @@ namespace Yontalane.Dialog
         /// </summary>
         /// <param name="line">The line data to initialize the dialog UI with.</param>
         /// <param name="lineCompleteCallback">The callback to invoke when the line is complete.</param>
-        [Obsolete("DialogUI has been moved to a separate package and will be removed from the Dialog package in a future update. Please replace thie component in your project with the DialogUI component found in the DialogUGUI package.")]
+        [Obsolete("DialogUI has been moved to a separate package and will be removed from the Dialog package in a future update. Please replace this component in your project with the DialogUI component found in the DialogUGUI package.")]
         public void Initiate(LineData line, Action<string> lineCompleteCallback, Func<string, string> replaceInlineText)
         {
             s_inlineImageReplacementPostProcessingInfo.Clear();
